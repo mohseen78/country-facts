efe3f4523d6bf68ba1b89e9c10f86ac48426cb22
Abdul Mohseen <abdulmohiseen78@gmail.com>
2019-12-10 05:10:55 +0530

    adding-layouts-navigation

---
+++ app/src/main/java/com/mohseen78/countryfacts/ui/FactsFragment.kt
@@ -0,0 +1,25 @@
+package com.mohseen78.countryfacts.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.mohseen78.countryfacts.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FactsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_facts, container, false)
+    }
+
+
+}
